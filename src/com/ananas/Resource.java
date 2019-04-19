901e5f116944fc200bf5f31dd1cd5c37c4f13eea
Bjornir90 <celestincollin@gmail.com>
2019-04-19 11:03:10 +0200

    Create server and Resource

--- src/com/ananas/Resource.java
+++ src/com/ananas/Resource.java
@@ -1,8 +1,27 @@
 package com.ananas;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-@Path("/hello")
+
 public class Resource {
-    
+
+    @GET
+    @Produces("text/plain")
+    @Path("/hello")
+    public String sayHello(@QueryParam("nom") String name){
+        return "Hello " + name;
+    }
+
+    @GET
+    @Produces("image/jpg")
+    @Path("/image")
+    public InputStream getImage() throws IOException {
+        return new FileInputStream("./files/Koala.jpg");
+    }
 }
